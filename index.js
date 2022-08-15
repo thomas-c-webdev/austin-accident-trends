859aff89de3230598a5f1b80dafadb1dcd1794b5
Thomas <tommy.czernek@gmail.com>
2022-08-15 11:19:59 -0500

    Wrote the HTML

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
   L.mapquest.key = 'xeJ8GxkvMW4NGA0DeW2Kqg3F5XkEPxNW';
 
+
+fetch('http://www.mapquestapi.com/traffic/v2/incidents?key=KEY&inFormat=json&outFormat=json')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+
+
+
+
+
+  /*
 // 'map' refers to a <div> element with the ID map
 L.mapquest.map('map', {
   center: [37.7749, -122.4194],
   layers: L.mapquest.tileLayer('map'),
   zoom: 12
 });
+*/
